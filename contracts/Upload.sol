a4a143eabd013839b5b65ce47352b6dfa6c9f31b
Navaneeth Suresh <navaneeth22yt@gmail.com>
2023-06-04 14:17:38 +0530

    solidity changes

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -1,36 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Upload {
     struct Access {
         address user;
-        bool access; // true or false
+        bool access; //true or false
     }
-
-    struct UrlObject {
-        string url1;
-        string url2;
-    }
-
-    mapping(address => UrlObject[]) value;
+    mapping(address => string[]) value;
     mapping(address => mapping(address => bool)) ownership;
     mapping(address => Access[]) accessList;
     mapping(address => mapping(address => bool)) previousData;
 
-    function add(
-        address _user,
-        string memory url1,
-        string memory url2
-    ) external {
-        UrlObject memory newObject = UrlObject(url1, url2);
-        value[_user].push(newObject);
+    function add(address _user, string memory url) external {
+        value[_user].push(url);
     }
 
     function allow(address user) external {
+        //def
         ownership[msg.sender][user] = true;
         if (previousData[msg.sender][user]) {
-            for (uint256 i = 0; i < accessList[msg.sender].length; i++) {
+            for (uint i = 0; i < accessList[msg.sender].length; i++) {
                 if (accessList[msg.sender][i].user == user) {
                     accessList[msg.sender][i].access = true;
                 }
@@ -43,14 +32,14 @@ contract Upload {
 
     function disallow(address user) public {
         ownership[msg.sender][user] = false;
-        for (uint256 i = 0; i < accessList[msg.sender].length; i++) {
+        for (uint i = 0; i < accessList[msg.sender].length; i++) {
             if (accessList[msg.sender][i].user == user) {
                 accessList[msg.sender][i].access = false;
             }
         }
     }
 
-    function display(address _user) external view returns (UrlObject[] memory) {
+    function display(address _user) external view returns (string[] memory) {
         require(
             _user == msg.sender || ownership[_user][msg.sender],
             "You don't have access"
