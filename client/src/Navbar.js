c02611dfb749a3bfb7164ba63874bcbc55757148
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-06-04 19:34:34 +0530

    gg

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -148,34 +148,20 @@ const MobileNav = ({ onOpen, ...rest }) => {
               <HStack>
                 <Avatar
                   size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+                  
                 />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
                   spacing="1px"
                   ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
+                  <Text fontSize="sm">Elon Musk</Text>
                   <Text fontSize="xs" color="gray.600">
                     User
                   </Text>
                 </VStack>
-                {/* <Box display={{ base: 'none', md: 'flex' }}>
-                  <FiChevronDown />
-                </Box> */}
               </HStack>
             </MenuButton>
-            {/* <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList> */}
           </Menu>
         </Flex>
       </HStack>
